089a3631a84785ace723e4455927080760ade89e
Abigail White <abigailwhite213@gmail.com>
2017-04-04 23:09:41 -0400

    added handlebars js and json for individual projects

---
+++ public/scripts/model/indivProjectsRender.js
@@ -0,0 +1,53 @@
+'use strict';
+(function(module) {
+
+  let projectView = {};
+
+  function Projects (opts) {
+    this.title = opts.title;
+    this.concept = opts.concept;
+    this.strategy = opts.strategy;
+    this.resources = opts.resources;
+    this.imgUrl = opts.imgUrl;
+    this.repoUrl = opts.repoUrl;
+    this.siteUrl = opts.siteUrl;
+    this.contributions = opts.contributions;
+    this.date = opts.date;
+  }
+
+  Projects.all = [];
+
+  Projects.prototype.toHtml = function() {
+    let template = Handlebars.compile($('#individual-template').text());
+    return template(this);
+  };
+
+  Projects.loadAll = function(projectsData) {
+    projectsData.forEach(function(ele) {
+      Projects.all.push(new Projects(ele));
+    });
+  }
+
+  projectView.projectsRender = function() {
+    Projects.all.forEach(function(a) {
+      $('#individual').append(a.toHtml());
+    });
+  };
+
+  // Render projects
+  Projects.fetchAll = function(callback) {
+      $.getJSON('/data/individualProjects.json')
+      .then(function(projectsData) {
+        Projects.loadAll(projectsData);
+        localStorage.setItem('projectsData', JSON.stringify(projectsData));
+        projectView.projectsRender();
+      })
+      .then(function() {
+        if (callback)
+          callback();
+      })
+    };
+
+  Projects.fetchAll();
+  module.Projects = Projects;
+}) (window);
